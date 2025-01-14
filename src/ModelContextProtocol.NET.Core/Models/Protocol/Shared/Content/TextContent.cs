798d5d061846fba559b71c337aae422620a50816
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-01-14 23:02:51 +0800

    feat(core): add convenience operators for `TextContent`

--- src/ModelContextProtocol.NET.Core/Models/Protocol/Shared/Content/TextContent.cs
+++ src/ModelContextProtocol.NET.Core/Models/Protocol/Shared/Content/TextContent.cs
@@ -1,7 +1,14 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+
 namespace ModelContextProtocol.NET.Core.Models.Protocol.Shared.Content;
 
 public record TextContent : Annotated
 {
     public string Type => "text";
     public required string Text { get; init; }
+
+    public static implicit operator Annotated[](TextContent textContent) => [textContent];
+
+    public static implicit operator TextContent(string text) => new TextContent { Text = text };
 }
