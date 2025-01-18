d35b5faca955735a063ce2495cf24fdedd4c13ee
salty-flower <95509524+salty-flower@users.noreply.github.com>
2025-01-18 17:48:30 +0800

    refactor: use new registry style

--- src/ModelContextProtocol.NET.Demo.Calculator/Program.cs
+++ src/ModelContextProtocol.NET.Demo.Calculator/Program.cs
@@ -18,54 +18,42 @@ var seriLogger = new LoggerConfiguration()
     .CreateLogger();
 
 // Configure and build server
-var server = new McpServerBuilder(serverInfo)
-    .AddStdioTransport()
-    .ConfigureLogging(logging => logging.AddSerilog(seriLogger).SetMinimumLevel(LogLevel.Trace))
-    .ConfigureUserServices(services =>
-        // Add logging
-        services.AddLogging(builder =>
-        {
-            builder.AddSerilog(seriLogger);
-            builder.SetMinimumLevel(LogLevel.Trace);
-        })
-    )
-    .ConfigureTools(tools =>
-        tools
-        //.AddHandler<CalculatorToolHandler>()
-        .AddFunction(
-            name: "Calculator_function_flavor",
-            description: "Still performs basic arithmetic operations, but implemented in functional style",
-            parameterTypeInfo: CalculatorParametersJsonContext.Default.CalculatorParameters,
-            handler: (CalculatorParameters parameters, CancellationToken ct) =>
-                Task.FromResult(
-                    new CallToolResult
-                    {
-                        Content = (TextContent)
-                            (
-                                (double)(
-                                    parameters.Operation switch
-                                    {
-                                        CalculatorOperation.Add => parameters.A + parameters.B,
-                                        CalculatorOperation.Subtract => parameters.A - parameters.B,
-                                        CalculatorOperation.Multiply => parameters.A * parameters.B,
-                                        CalculatorOperation.Divide when parameters.B != 0
-                                            => parameters.A / parameters.B,
-                                        CalculatorOperation.Divide
-                                            => throw new DivideByZeroException(
-                                                "Cannot divide by zero"
-                                            ),
-                                        _
-                                            => throw new ArgumentException(
-                                                $"Unknown operation: {parameters.Operation}"
-                                            ),
-                                    }
-                                )
-                            ).ToString()
-                    }
-                )
+var builder = new McpServerBuilder(serverInfo).AddStdioTransport();
+builder.Services.AddLogging(builder =>
+    builder.AddSerilog(seriLogger).SetMinimumLevel(LogLevel.Trace)
+);
+builder.Tools.AddFunction(
+    name: "Calculator_function_flavor",
+    description: "Still performs basic arithmetic operations, but implemented in functional style",
+    parameterTypeInfo: CalculatorParametersJsonContext.Default.CalculatorParameters,
+    handler: (CalculatorParameters parameters, CancellationToken ct) =>
+        Task.FromResult(
+            new CallToolResult
+            {
+                Content = (TextContent)
+                    (
+                        (double)(
+                            parameters.Operation switch
+                            {
+                                CalculatorOperation.Add => parameters.A + parameters.B,
+                                CalculatorOperation.Subtract => parameters.A - parameters.B,
+                                CalculatorOperation.Multiply => parameters.A * parameters.B,
+                                CalculatorOperation.Divide when parameters.B != 0
+                                    => parameters.A / parameters.B,
+                                CalculatorOperation.Divide
+                                    => throw new DivideByZeroException("Cannot divide by zero"),
+                                _
+                                    => throw new ArgumentException(
+                                        $"Unknown operation: {parameters.Operation}"
+                                    ),
+                            }
+                        )
+                    ).ToString()
+            }
         )
-    )
-    .Build();
+);
+
+var server = builder.Build();
 
 try
 {
